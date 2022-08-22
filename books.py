cf733b2ad88830f29fe2de90ec83792fa5df214f
mrblue223 <96673202+mrblue223@users.noreply.github.com>
2022-08-21 23:52:17 -0400

    Update books.py

--- books.py
+++ books.py
@@ -3,6 +3,8 @@
 #LINUX
 https://b-ok.cc/book/2485331/e2d1cb (Linux bible)
 https://b-ok.cc/book/3653439/16c442?dsource=recommend (linux for hackers)
+https://b-ok.cc/book/3704056/43b3c7?dsource=recommend (the linux command line)
+
 #DEFENCE
 https://b-ok.cc/book/2867237/08799e (The art of invisibility by kevin mitnick)
 
@@ -11,11 +13,21 @@ https://b-ok.cc/book/3401544/518a0b (The web application hacker’s handbook: fi
 https://drive.google.com/file/d/0By8giYopjXKPQWJ5RGVMdVJKLTQ/view?resourcekey=0-AaLCOYm1SJN-LcJrTnJJ0w (hacker playbook 1)
 https://drive.google.com/file/d/0By8giYopjXKPUVVuNHBDbkU3VUE/view?resourcekey=0-E83lINRI7svyW_u1Kw2LuA (hacker playbook 2)
 https://pdfroom.com/books/the-hacker-playbook-3-practical-guide-to-penetration-testing/4zW5nWrxdNq (hacker playbook 3)
+https://b-ok.cc/book/3487236/79e007?dsource=recommend (attacking network protocols)
+https://b-ok.cc/book/5208551/37edd0?dsource=recommend (Real-World Bug Hunting)
+https://b-ok.cc/book/2482934/27a641?dsource=recommend (Penetration Testing)
   
 #MALWARE/CODING
 https://b-ok.cc/book/2950584/10f8a1?dsource=recommend (python and hacking made simple)
 https://b-ok.cc/book/2475649/baf4dc?dsource=recommend (black hat python)
+https://b-ok.cc/book/5286627/0e6558?dsource=recommend (malware, data science)
+https://b-ok.cc/book/4995978/7ebef5?dsource=recommend (rootkits and bootkits)
+https://b-ok.cc/book/1274723/254e0e?dsource=recommend (Practical Malware Analysis)
+
   
 #SOCIAL ENGINEERING
 https://b-ok.cc/book/866637/a8ebab (The art of social engineering)
+  
+#CRYPTOGRAPHY
+https://b-ok.cc/book/3399523/14ceb5?dsource=recommend (Serious Cryptography)
 
