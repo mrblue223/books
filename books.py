f447edce99498414542f017acd64083b3cd2d452
mrblue223 <96673202+mrblue223@users.noreply.github.com>
2022-08-21 23:40:21 -0400

    Update books.py

--- books.py
+++ books.py
@@ -1,11 +1,17 @@
 #ETHICAL HACKING
 
 #LINUX
-
+https://b-ok.cc/book/2485331/e2d1cb (Linux bible)
+  
 #DEFENCE
-https://b-ok.cc/book/2867237/08799e
+https://b-ok.cc/book/2867237/08799e (The art of invisibility by kevin mitnick)
+
 #OFFENCE
 
 #MALWARE/CODING
-https://b-ok.cc/book/2950584/10f8a1?dsource=recommend
-https://b-ok.cc/book/2475649/baf4dc?dsource=recommend
+https://b-ok.cc/book/2950584/10f8a1?dsource=recommend (python and hacking made simple)
+https://b-ok.cc/book/2475649/baf4dc?dsource=recommend (black hat python)
+  
+#SOCIAL ENGINEERING
+https://b-ok.cc/book/866637/a8ebab (The art of social engineering)
+
