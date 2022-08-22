e832a01149933d712074b0f5858df3b8f7f46538
mrblue223 <96673202+mrblue223@users.noreply.github.com>
2022-08-21 23:36:33 -0400

    Create books.py

---
+++ books.py
@@ -0,0 +1,11 @@
+#ETHICAL HACKING
+
+#LINUX
+
+#DEFENCE
+https://b-ok.cc/book/2867237/08799e
+#OFFENCE
+
+#MALWARE/CODING
+https://b-ok.cc/book/2950584/10f8a1?dsource=recommend
+https://b-ok.cc/book/2475649/baf4dc?dsource=recommend
