15eb5508f290141b13772a23035fb32187ed42dd
mrblue223 <96673202+mrblue223@users.noreply.github.com>
2022-08-21 23:46:13 -0400

    Update books.py

--- books.py
+++ books.py
@@ -2,12 +2,16 @@
 
 #LINUX
 https://b-ok.cc/book/2485331/e2d1cb (Linux bible)
-  
+https://b-ok.cc/book/3653439/16c442?dsource=recommend (linux for hackers)
 #DEFENCE
 https://b-ok.cc/book/2867237/08799e (The art of invisibility by kevin mitnick)
 
 #OFFENCE
-
+https://b-ok.cc/book/3401544/518a0b (The web application hacker’s handbook: finding and exploiting security flaws )
+https://drive.google.com/file/d/0By8giYopjXKPQWJ5RGVMdVJKLTQ/view?resourcekey=0-AaLCOYm1SJN-LcJrTnJJ0w (hacker playbook 1)
+https://drive.google.com/file/d/0By8giYopjXKPUVVuNHBDbkU3VUE/view?resourcekey=0-E83lINRI7svyW_u1Kw2LuA (hacker playbook 2)
+https://pdfroom.com/books/the-hacker-playbook-3-practical-guide-to-penetration-testing/4zW5nWrxdNq (hacker playbook 3)
+  
 #MALWARE/CODING
 https://b-ok.cc/book/2950584/10f8a1?dsource=recommend (python and hacking made simple)
 https://b-ok.cc/book/2475649/baf4dc?dsource=recommend (black hat python)
